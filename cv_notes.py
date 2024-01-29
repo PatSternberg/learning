b731edda98685126d08c69899ee1fe3cfcad4007
patrickskipworth <pskipworth@fastmail.com>
2024-01-29 14:48:48 +0000

    Add CV workshop notes

---
+++ cv_notes.py
@@ -0,0 +1,59 @@
+# +-+-+| CV NOTES |+-+-+
+# 
+# 
+# ==| GENERAL NOTES |==
+# 
+# = WHAT TO INCLUDE
+# Makers' Python-only approach: include other languages you know
+# but overall it's the Makers approach that is recruited on i.e.
+# Makers grads are able to keep learning and approach problems
+# the right way
+# 
+# Prioritise technical skills and experience e.g. project table
+# should be in a priority position. This also applies to language,
+# and includes in interviews, even if they are presented as
+# 'non-technical'
+# 
+# Include tech-critical soft / transferrable skills e.g. communication, 
+# time management and adaptability. 
+# 
+# Include skills that complement software engineering (e.g. data analysis,
+# design or a background in a particular field)
+# 
+# 
+# ==| FORMAT / STRUCTURE |==
+# 
+# = TEMPLATES, PRESENTATION AND FONTS =
+# Tailor a CV to be read by an AI or inexperience recruiter
+# e.g. by using a template or including lots of keywords
+# 
+# CV should be 1-2 pages, if there's stuff to showcase, do stretch
+# it to 2 pages, but make sure the info you want the recruiter to
+# read is on page 1
+# 
+# Use simple and common fonts (Calibri) - a system may not be able 
+# to read it otherwise, and it might just e.g. return symbols
+# 
+# = ABOUT ME / PERSONAL PROFILE =
+# About me section is how to differentiate yourself from others based
+# on your experience and learning at Makers
+#   > Should be short and showcase your experience you will bring 
+#     into role
+#   > Should cover why you made the move to tech
+# 
+# = PROJECT TABLE =
+# Include 2-4 projects in a projects table (with link to project, list
+# tech stack and project description)
+# 
+# = WORK EXPERIENCE / EDUCATION =
+# Makers experience can be in work experience or in education (but
+# either way on the first page)
+#   > Include university but no earlier
+#   > Include technical certifications (including Makers)
+#   > Include any online courses completed as well
+# 
+# = INTERESTS / HOBBIES =
+# Highlight any links to tech
+# 
+#
+#
